09aba968b8dc02babb99cc18012f7e49c3c5020a
Yaming Gong <ymgongcn@gmail.com>
2022-11-25 21:12:33 +0800

    feat: add ethereum go client

---
+++ ethereum/go/client.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://cloudflare-eth.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("we have a connection")
+	_ = client // we'll use this in the upcoming sections
+}
