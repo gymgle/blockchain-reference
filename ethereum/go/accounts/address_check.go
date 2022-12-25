6cfabf7791ea46db36c00fcd66fd7151746bda06
Yaming Gong <ymgongcn@gmail.com>
2022-12-25 16:46:09 +0800

    feat: ethereum go accounts address check

---
+++ ethereum/go/accounts/address_check.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"regexp"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	// This section will describe how to validate an address and determine if it's a smart contract address.
+
+	// We can use a simple regular expression to check if the ethereum address is valid.
+	re := regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
+
+	fmt.Printf("is valid: %v\n", re.MatchString("0x323b5d4c32345ced77393b3530b1eed0f346429d")) // is valid: true
+	fmt.Printf("is valid: %v\n", re.MatchString("0xZYXb5d4c32345ced77393b3530b1eed0f346429d")) // is valid: false
+
+	// Check if Address is an Account or a Smart Contract
+	client, err := ethclient.Dial("https://cloudflare-eth.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 0x Protocol Token (ZRX) smart contract address
+	address := common.HexToAddress("0xe41d2489571d322189246dafa5ebde1f4699f498")
+	bytecode, err := client.CodeAt(context.Background(), address, nil) // nil is latest block
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	isContract := len(bytecode) > 0
+
+	fmt.Printf("is contract: %v\n", isContract) // is contract: true
+
+	// if there's no bytecode at the address then we know that it's not a smart contract and it's a standard ethereum account.
+	// a random user account address
+	address = common.HexToAddress("0x8e215d06ea7ec1fdb4fc5fd21768f4b34ee92ef4")
+	bytecode, err = client.CodeAt(context.Background(), address, nil) // nil is latest block
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	isContract = len(bytecode) > 0
+
+	fmt.Printf("is contract: %v\n", isContract) // is contract: false
+}
